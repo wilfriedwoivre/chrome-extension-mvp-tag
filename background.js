83d9edd9be685339e3b21025ee05097eca85537b
Wilfried Woivré <wilfried.woivre@gmail.com>
2024-04-02 20:48:25 +0200

    update

--- background.js
+++ background.js
@@ -4,34 +4,45 @@ chrome.webNavigation.onDOMContentLoaded.addListener(async ({ tabId, url }) => {
             target: { tabId },
             func: AddMVPTag
         }).then(() => console.log("injected a function"));
-    } else {
-        return;
     }
 });
 
 
 function AddMVPTag() {
-    var currentUrl = window.location.href;
-    newUrl = "";
-    if (currentUrl.indexOf("?") == -1) {
-        newUrl = currentUrl + "?WT.mc_id=AZ-MVP-4039694";
-    }
-    else {
-        tagIndex = currentUrl.indexOf("WT.mc_id=");
-        if (tagIndex == -1) {
-            newUrl = currentUrl + "&WT.mc_id=AZ-MVP-4039694";
-        }
-        else {
-            nextParameters = currentUrl.indexOf("&", tagIndex);
-            if (nextParameters == -1) { nextParameters = currentUrl.length }
-            newUrl = currentUrl.replace(currentUrl.slice(tagIndex, nextParameters), "WT.mc_id=AZ-MVP-4039694")
-        }
-    }
+    console.log("HREF " + window.location.href);
 
-    console.log("Redirecting: " + currentUrl + " to " + newUrl);
-    if (currentUrl !== newUrl) {
-        window.location.replace(newUrl);
-    }
+    var ignoreList = [
+        "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
+        "https://learn.microsoft.com/en-us/docs.theme/master/en-us/_themes/global/identity-profile.html",
+        "https://learn.microsoft.com/_themes/docs.theme/master/en-us/_themes/global/identity-profile.html"
+    ];
 
+    if (ignoreList.includes(window.location.href)) {
+        console.log("Ignoring " + window.location.href);
+    } else {
+        if (window.location.origin == "https://learn.microsoft.com") {
+            var currentUrl = window.location.href;
+            newUrl = "";
+            if (currentUrl.indexOf("?") == -1) {
+                newUrl = currentUrl + "?WT.mc_id=AZ-MVP-4039694";
+            }
+            else {
+                tagIndex = currentUrl.indexOf("WT.mc_id=");
+                if (tagIndex == -1) {
+                    newUrl = currentUrl + "&WT.mc_id=AZ-MVP-4039694";
+                }
+                else {
+                    nextParameters = currentUrl.indexOf("&", tagIndex);
+                    if (nextParameters == -1) { nextParameters = currentUrl.length }
+                    newUrl = currentUrl.replace(currentUrl.slice(tagIndex, nextParameters), "WT.mc_id=AZ-MVP-4039694")
+                }
+            }
+
+            if (currentUrl !== newUrl) {
+                console.log("Redirecting: " + currentUrl + " to " + newUrl);
+                window.location.replace(newUrl);
+            }
+        }
+    }
 }
 
